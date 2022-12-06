f68cf2d2a25503286108758e9e6eb04e42bd31a7
Satmei Ainun <satmei.ainun@gmail.com>
2022-12-06 16:02:22 +0700

    add frameworks

---
+++ Sephora-Mobile/Sephora-Mobile/Moduls/PopUp/RegisterPopUpViewController.swift
@@ -0,0 +1,9 @@
+//
+//  RegisterPopUpViewController.swift
+//  Sephora-Mobile
+//
+//  Created by Key on 02/12/22.
+//  Copyright © 2022 Key. All rights reserved.
+//
+
+import Foundation
---
+++ Sephora-Mobile/Sephora-Mobile/Moduls/PopUpLogin/LoginPopUpViewController.swift
@@ -0,0 +1,9 @@
+//
+//  LoginPopUpViewController.swift
+//  Sephora-Mobile
+//
+//  Created by Key on 02/12/22.
+//  Copyright © 2022 Key. All rights reserved.
+//
+
+import Foundation
